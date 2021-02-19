0f1f703f7a3c024ebc01113e236c54392a8cc19d
mikalai-ka <1mikalai.dev@gmail.com>
2021-02-19 23:22:42 +0300

    Update server.py
    modify variable and few methods

--- server.py
+++ server.py
@@ -9,7 +9,7 @@ import game
 SERVER_MESSAGE_PREFIX = 'System'
 
 # Chat commands:
-HELP = '##commands'
+HELP = '##help'
 END_CHAT_CMD = '##stop'
 GAME_CMD = '##game'
 PARTICIPANT_COUNT = '##number'
@@ -18,12 +18,12 @@ SHOW_TIME = '##time'
 
 COMMANDS_LIST = {HELP, GAME_CMD, PARTICIPANT_COUNT, PARTICIPANTS, SHOW_TIME, END_CHAT_CMD}
 
-MAN = f"\n{HELP} - show chat commands" \
-      f"\n{GAME_CMD} - start Rock Paper Scissors game" \
-      f"\n{PARTICIPANT_COUNT} - number of users in chat" \
-      f"\n{PARTICIPANTS} - name of users in chats" \
-      f"\n{SHOW_TIME} - current time" \
-      f"\n{END_CHAT_CMD} - exit from the chat"
+CMD_DESC = f"\n{HELP} - show chat commands" \
+          f"\n{GAME_CMD} - start Rock Paper Scissors game" \
+          f"\n{PARTICIPANT_COUNT} - number of users in chat" \
+          f"\n{PARTICIPANTS} - name of users in chats" \
+          f"\n{SHOW_TIME} - current time" \
+          f"\n{END_CHAT_CMD} - exit from the chat"
 
 HOST = socket.gethostname()
 PORT = 55500
@@ -55,15 +55,6 @@ def send_message(user_id, message_text, prefix=SERVER_MESSAGE_PREFIX, to_self=0)
                 recipient.send(message)
 
 
-def send_chat_message(user_id, message_text):
-    pass
-
-
-def get_message(recipient):
-    recv_message = recipient.recv(1024)
-    return recv_message.decode()
-
-
 def log(*log_line):
     now = time.time()
     milliseconds = '%03d' % int((now - int(now)) * 1000)
@@ -77,7 +68,7 @@ def start():
     while 1:
         server.listen()
         conn, addr = server.accept()
-        user_name = get_message(conn)
+        user_name = conn.recv(1024).decode()
         user_id = add_client_info(conn, user_name)
         log("[USER_CONNECTION] new user has been connected. user_id: ", str(user_id), ", user_name:", user_name)
         thread = threading.Thread(target=handle_client, args=(conn, user_name, user_id))
@@ -99,15 +90,14 @@ def handle_client(conn, user_name, user_id):
     log(f"[CHAT_JOIN] user {user_name} joins to the chat")
     chat_participants[user_id] = user_name
     while 1:
-        new_message = get_message(conn)
-
+        new_message = conn.recv(1024).decode()
         if new_message.startswith('##'):
-            if new_message == HELP:
-                send_message(user_id, MAN, to_self=1)
 
-            if new_message == END_CHAT_CMD:
-                message = user_name + ' has left the chat'
-                send_message(user_id, message)
+            if new_message == HELP:
+                send_message(user_id, CMD_DESC, to_self=1)
+
+            elif new_message == END_CHAT_CMD:
+                send_message(user_id, f"{user_name} has left the chat")
                 send_message(user_id, 'You leave the chat', to_self=1)
                 log("[USER_END_CHAT] user: ", user_name)
                 clients.pop(user_id)
@@ -115,22 +105,24 @@ def handle_client(conn, user_name, user_id):
                 conn.close
                 break
 
-            if new_message == GAME_CMD:
+            elif new_message == GAME_CMD:
                 log(f"[GAME_START] user {user_name} started RPS")
                 send_message(user_id, 'Rock Paper Scissors game has been started', to_self=1)
                 game.rock_paper_scissors(conn)
                 send_message(user_id, 'Now you can continue chat', to_self=1)
                 log(f'[GAME_END] user {user_name} ended RPS')
 
-            if new_message == PARTICIPANT_COUNT:
+            elif new_message == PARTICIPANT_COUNT:
                 pass
-            if new_message == PARTICIPANTS:
+            elif new_message == PARTICIPANTS:
                 pass
-            if new_message == SHOW_TIME:
+            elif new_message == SHOW_TIME:
                 pass
 
             else:
                 send_message(user_id, f'Invalid command. Type "{HELP}" to see chat commands.', to_self=1)
+
+            log("[COMMAND_MESSAGE] user: ", user_name, ", command_text: ", new_message)
 
         else:
             log("[NEW_MESSAGE] user: ", user_name, ", message_text: ", new_message)
