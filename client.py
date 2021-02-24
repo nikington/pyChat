4c35299700940d1612075da302194052dcacea68
aka DeM <Demka>
2021-02-24 20:58:40 +0300

    small changes

--- client.py
+++ client.py
@@ -4,9 +4,11 @@ import threading
 HOST = socket.gethostname()
 PORT = 55500
 
+
 def close_chat(client):
     client.close()
     return 0
+
 
 def get_message():
     connection = 1
@@ -14,25 +16,27 @@ def get_message():
         incoming_message = s.recv(1024).decode()
         print(incoming_message)
 
+
 def send_message():
     connection = 1
     while connection:
         new_message_text = input(str())
         s.send(new_message_text.encode())
 
+
 s = socket.socket()
-s.connect((HOST,PORT))
+s.connect(HOST, PORT)
 print("Client app is started.")
-user_exist=True
+user_exist = True
 while user_exist:
     print("Please enter your nickname:")
     new_message_text = input(str())
     s.send(new_message_text.encode())
-    user_exist=False
+    user_exist = False
     incoming_message = s.recv(1024).decode()
     print(incoming_message)
-    if incoming_message=="System: The user with this name already exist":
-        user_exist=True
+    if incoming_message == "System: The user with this name already exist":
+        user_exist = True
 thread = threading.Thread(target=get_message, args=())
 thread.start()
 thread = threading.Thread(target=send_message, args=())
--- server.py
+++ server.py
@@ -19,11 +19,11 @@ SHOW_TIME = '##time'
 COMMANDS_LIST = {HELP, GAME_CMD, PARTICIPANT_COUNT, PARTICIPANTS, SHOW_TIME, END_CHAT_CMD}
 
 CMD_DESC = f"\n{HELP} - show chat commands" \
-          f"\n{GAME_CMD} - start Rock Paper Scissors game" \
-          f"\n{PARTICIPANT_COUNT} - number of users in chat" \
-          f"\n{PARTICIPANTS} - name of users in chats" \
-          f"\n{SHOW_TIME} - current time" \
-          f"\n{END_CHAT_CMD} - exit from the chat"
+           f"\n{GAME_CMD} - start Rock Paper Scissors game" \
+           f"\n{PARTICIPANT_COUNT} - number of users in chat" \
+           f"\n{PARTICIPANTS} - name of users in chats" \
+           f"\n{SHOW_TIME} - current time" \
+           f"\n{END_CHAT_CMD} - exit from the chat"
 
 HOST = socket.gethostname()
 PORT = 55500
@@ -31,6 +31,7 @@ server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server.bind((HOST, PORT))
 
 clients = []
+
 
 def send_message(user_id, message_text, prefix=SERVER_MESSAGE_PREFIX, to_self=0):
     message = prefix + ': ' + message_text
@@ -44,42 +45,47 @@ def send_message(user_id, message_text, prefix=SERVER_MESSAGE_PREFIX, to_self=0)
                 recipient = clients[client_id]['connection']
                 recipient.send(message)
 
+
 def log(*log_line):
     print(datetime.now().isoformat(sep=' ', timespec='milliseconds'), "-", ' '.join(log_line))
+
 
 def start():
     log("[START] server has been started")
     while True:
         server.listen()
-        conn, addr = server.accept()        
+        conn, addr = server.accept()
         thread = threading.Thread(target=handle_client, args=(conn,))
         thread.start()
-        
+
+
 def check_user_name(conn):
-    exist=True
+    exist = True
     while exist:
         user_name = conn.recv(128).decode()
-        exist=False
+        exist = False
         for client in clients:
-            if user_name==client['name']:
-                exist=True
+            if user_name == client['name']:
+                exist = True
                 conn.send("System: The user with this name already exist".encode())
     return user_name
 
-def user_initial_steps(id):
-    user_name=clients[id]['name']
-    log(f"[USER_CONNECTION] new user has been connected. user_id: {id}, user_name: {user_name}")
-    send_message(id, f"Greetings, {user_name}!", to_self=1)
+
+def user_initial_steps(user_id):
+    user_name = clients[user_id]['name']
+    log(f"[USER_CONNECTION] new user has been connected. user_id: {user_id}, user_name: {user_name}")
+    send_message(user_id, f"Greetings, {user_name}!", to_self=1)
     message = "You are connected to the chat. Now you can send messages." \
               f'\nType "{HELP}" to see chat commands.'
-    send_message(id, message, to_self=1)
-    send_message(id, user_name + ' has joined to the chat')
+    send_message(user_id, message, to_self=1)
+    send_message(user_id, user_name + ' has joined to the chat')
     log(f"[CHAT_JOIN] user {user_name} joins to the chat")
 
-def handle_client(conn):    
-    user_name=check_user_name(conn)
+
+def handle_client(conn):
+    user_name = check_user_name(conn)
     clients.append({'connection': conn, 'name': user_name})
-    user_id=len(clients)-1
+    user_id = len(clients) - 1
     user_initial_steps(user_id)
 
     while True:
@@ -108,13 +114,13 @@ def handle_client(conn):
                 send_message(user_id, f"Number of Participants - {len(clients)}", to_self=1)
 
             elif new_message == PARTICIPANTS:
-                users_list='\nChat participants: \n'
+                users_list = '\nChat participants: \n'
                 for client in clients:
-                    users_list+=client['name']+'\n'
+                    users_list += client['name'] + '\n'
                 send_message(user_id, users_list, to_self=1)
 
             elif new_message == SHOW_TIME:
-                send_message(user_id, "Current time "+datetime.now().isoformat(sep=' ', timespec='seconds'), to_self=1)
+                send_message(user_id, "Current time " + datetime.now().isoformat(sep=' ', timespec='seconds'), to_self=1)
 
             else:
                 send_message(user_id, f'Invalid command. Type "{HELP}" to see chat commands.', to_self=1)
@@ -124,5 +130,6 @@ def handle_client(conn):
         else:
             send_message(user_id, new_message, user_name)
             log(f"[NEW_MESSAGE] user: {user_name} message_text: {new_message}")
+
 
 start()
