4f6c5d2accd0a528c9dd2e39510acb339da05a97
aka DeM <Demka>
2021-02-24 23:19:48 +0300

    Solved issue #5

--- client.py
+++ client.py
@@ -1,5 +1,7 @@
 import socket
 import threading
+import sys
+import os
 
 HOST = socket.gethostname()
 PORT = 55500
@@ -24,8 +26,27 @@ def send_message():
         s.send(new_message_text.encode())
 
 
-s = socket.socket()
-s.connect(HOST, PORT)
+os.system('cls' if os.name == 'nt' else 'clear')
+try:
+    HOST = sys.argv[1]
+    PORT = int(sys.argv[2])
+except:
+    print("Server parametrs were not provided. Default accepted.")
+
+tc = True
+while tc:
+    tc = False
+    try:
+        s = socket.socket()
+        s.connect((HOST, PORT))
+    except TimeoutError:
+        print(f"Server {HOST} on {PORT} is not available.")
+        tc = True
+        print("Enter Host (IP):")
+        HOST = str(input())
+        print("Enter port number")
+        PORT = int(input())
+
 print("Client app is started.")
 user_exist = True
 while user_exist:
@@ -37,6 +58,7 @@ while user_exist:
     print(incoming_message)
     if incoming_message == "System: The user with this name already exist":
         user_exist = True
+os.system('cls' if os.name == 'nt' else 'clear')
 thread = threading.Thread(target=get_message, args=())
 thread.start()
 thread = threading.Thread(target=send_message, args=())
